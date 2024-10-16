af003b3ca25fd5d45c874bb579475fd63e9dd6e4
nazarshtiyuk <nazarshtiyuk@gmail.com>
2024-10-16 23:09:27 +0300

    Added db connection, custom error handler, user store

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,18 +1,37 @@
 package api
 
 import (
+	"context"
+
+	"github.com/NazarShtiyuk/hotel-reservation/db"
 	"github.com/NazarShtiyuk/hotel-reservation/types"
 	"github.com/gofiber/fiber/v2"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUserByID(c *fiber.Ctx) error {
+	ctx := context.Background()
+	id := c.Params("id")
+	user, err := h.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	u := types.User{
 		FirstName: "James",
 		LastName:  "At the watercooler",
 	}
 	return c.JSON(u)
-}
-
-func HandleGetUserByID(c *fiber.Ctx) error {
-	return c.JSON("James")
 }
