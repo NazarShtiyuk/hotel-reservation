e0c8ce49e15a6ee8dfe7389fb4f3bfcf35eefbb1
nazarshtiyuk <nazarshtiyuk@gmail.com>
2024-10-27 01:55:06 +0300

    Added new handlers(delete, update)

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,9 +1,14 @@
 package api
 
 import (
+	"errors"
+
 	"github.com/NazarShtiyuk/hotel-reservation/db"
 	"github.com/NazarShtiyuk/hotel-reservation/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserHandler struct {
@@ -20,7 +25,9 @@ func (h *UserHandler) HandleGetUserByID(c *fiber.Ctx) error {
 	id := c.Params("id")
 	user, err := h.userStore.GetUserByID(c.Context(), id)
 	if err != nil {
-		return err
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.JSON(map[string]string{"msg": "not found"})
+		}
 	}
 	return c.JSON(user)
 }
@@ -31,6 +38,35 @@ func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 		return err
 	}
 	return c.JSON(users)
+}
+
+func (h *UserHandler) HandlePutUser(c *fiber.Ctx) error {
+	userID := c.Params("id")
+
+	oid, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		return err
+	}
+
+	var params types.UpdateUserParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": oid}
+	if err := h.userStore.UpdateUser(c.Context(), filter, params); err != nil {
+		return err
+	}
+
+	return c.JSON(map[string]string{"updated": userID})
+}
+
+func (h *UserHandler) HandleDeleteUser(c *fiber.Ctx) error {
+	userID := c.Params("id")
+	if err := h.userStore.DeleteUser(c.Context(), userID); err != nil {
+		return err
+	}
+	return c.JSON(map[string]string{"deleted": userID})
 }
 
 func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
